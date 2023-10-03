1ae5bdcc9c9e20e491a0f4bb4382658c24edf0c4
Sharyk Shaikh <shariqshaikh1235@gmail.com>
2023-10-03 07:08:29 +0530

    Level 2

--- Contamination/Assets/Scripts/Platforms/CollapsingPlatform.cs
+++ Contamination/Assets/Scripts/Platforms/CollapsingPlatform.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CollapsingPlatform : MonoBehaviour
 {
+    private GameObject CrackedWall;
+    [SerializeField] Collider2D Hydraulicscollider;
+    [SerializeField] Rigidbody2D rb;
+    public float Force;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CrackedWall = GameObject.FindGameObjectWithTag("Cracked Wall");
+
+        rb.velocity = new Vector2(10,rb.velocity.y) * Force;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.CompareTag("Cracked Wall"))
+        {
+            Destroy(gameObject);
+            Destroy(CrackedWall);    
+        }
         
+
     }
 }
--- Contamination/Assets/Scripts/Player/KillPlayer.cs
+++ Contamination/Assets/Scripts/Player/KillPlayer.cs
@@ -8,20 +8,20 @@ using UnityEngine.SceneManagement;
 
 public class KillPlayer : MonoBehaviour
 {
-    Vector2 startPosition;
     public int sceneBuildIndex;
 
-    private void Start()
-    {
-        startPosition = transform.position;
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Obstacle"))
         {
             Respawn();
         }
+        if (collision.CompareTag("Hydraulic"))
+        {
+            Respawn();
+        }
     }
+
     public void Respawn()
     {
         SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
